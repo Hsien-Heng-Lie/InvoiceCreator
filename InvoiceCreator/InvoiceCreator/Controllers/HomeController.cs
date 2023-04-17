d6ee3a90036fa5972ee7ac005af4f85960b39999
StevenPintoVC <steven.pinto@vcontractor.co.za>
2023-04-17 08:53:41 +0200

    Frontend project created, right now view invoices is done, and general home page is created.

---
+++ InvoiceCreator/InvoiceCreator/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using InvoiceCreator.Models;
+using InvoiceCreator.StaticData;
+using InvoiceCreator.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Transactions;
+
+namespace InvoiceCreator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private List<TransactionsModel> transactions;
+        private PaginationHelper pagination;
+
+        
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            transactions =  TransactionsData.Transactions;
+            pagination = new PaginationHelper(5, 1, transactions.Count);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult ViewTransaction(string searchString, string currentFilter, int? page)
+        {
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                transactions = transactions.Where(s => s.Student.FirstName.Contains(searchString) || s.Student.LastName.Contains(searchString)).ToList();
+            }
+
+            ViewBag.PageNumber = pagination._currentPage;
+            return View(pagination.GetPageByNumber(transactions,pagination._currentPage));
+        }
+
+        public IActionResult PaginationNext() 
+        {
+
+            pagination._currentPage = pagination._currentPage + 1;
+            ViewBag.PageNumber = pagination._currentPage;
+            return View("ViewTransaction",pagination.GetPageByNumber(transactions, pagination._currentPage));
+        }
+
+        public IActionResult PaginationPrevious()
+        {
+            pagination._currentPage--; 
+            ViewBag.PageNumber = pagination._currentPage;
+            return View("ViewTransaction", pagination.GetPageByNumber(transactions, pagination._currentPage));
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InvoiceCreator/InvoiceCreator/Models/LevelUpModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoiceCreator.Models
+{
+    public class LevelUpModel
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; } 
+    }
+}
