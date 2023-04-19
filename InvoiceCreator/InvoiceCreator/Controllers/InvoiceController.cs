77098adf1124206d3df9cd210fbd7bb5276f6251
StevenPintoVC <steven.pinto@vcontractor.co.za>
2023-04-19 11:33:21 +0200

    Fixed pagination and Action call for view transactions.

--- InvoiceCreator/InvoiceCreator/Controllers/InvoiceController.cs
+++ InvoiceCreator/InvoiceCreator/Controllers/InvoiceController.cs
@@ -1,8 +1,8 @@
 ﻿using InvoiceCreator.Controllers;
-using InvoiceCreator.Helpers;
 using InvoiceCreator.Models;
 using InvoiceCreator.StaticData;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace InvoiceCreatorFrontend.Controllers
 {
@@ -11,28 +11,11 @@ namespace InvoiceCreatorFrontend.Controllers
 
         private readonly ILogger<HomeController> _logger;
         private List<TransactionsModel> transactions;
-        private PaginationHelper pagination;
-
-        public IActionResult PaginationNext()
-        {
-
-            pagination._currentPage = pagination._currentPage + 1;
-            ViewBag.PageNumber = pagination._currentPage;
-            return View("ViewTransaction", pagination.GetPageByNumber(transactions, pagination._currentPage));
-        }
-
-        public IActionResult PaginationPrevious()
-        {
-            pagination._currentPage--;
-            ViewBag.PageNumber = pagination._currentPage;
-            return View("ViewTransaction", pagination.GetPageByNumber(transactions, pagination._currentPage));
-        }
 
         public InvoiceController(ILogger<HomeController> logger)
         {
             _logger = logger;
             transactions = TransactionsData.Transactions;
-            pagination = new PaginationHelper(5, 1, transactions.Count);
         }
 
         public IActionResult Index()
@@ -63,8 +46,11 @@ namespace InvoiceCreatorFrontend.Controllers
                 transactions = transactions.Where(s => s.Student.FirstName.Contains(searchString) || s.Student.LastName.Contains(searchString)).ToList();
             }
 
-            ViewBag.PageNumber = pagination._currentPage;
-            return View(pagination.GetPageByNumber(transactions, pagination._currentPage));
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+
+            return View(transactions.ToPagedList(pageNumber, pageSize));
+            //return View(pagination.GetPageByNumber(transactions, pagination._currentPage));
         }
 
         public IActionResult CreateInvoice()
