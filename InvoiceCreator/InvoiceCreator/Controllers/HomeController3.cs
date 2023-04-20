ef2998c5e330af340f70b8538ab7583b77bb437f
george_pauer <george.pauer1@vcontractor.co.za>
2023-04-20 13:01:03 +0200

    partial classes

--- InvoiceCreator/InvoiceCreator/Controllers/HomeController3.cs
+++ InvoiceCreator/InvoiceCreator/Controllers/HomeController3.cs
@@ -4,11 +4,8 @@ namespace InvoiceCreator.Controllers
 {
     public partial class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Privacy()
         {
-            // Set navbar indicator location
-            ViewBag.indicatorOpacity = "0";
-            
             return View();
         }
     }
--- InvoiceCreator/InvoiceCreator/Controllers/InvoiceController3.cs
+++ InvoiceCreator/InvoiceCreator/Controllers/InvoiceController3.cs
@@ -8,28 +8,12 @@ namespace InvoiceCreatorFrontend.Controllers
 {
     public partial class InvoiceController : Controller
     {
-
-        private readonly ILogger<HomeController> _logger;
-        private List<TransactionsModel> transactions;
-
-        public InvoiceController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-            transactions = TransactionsData.Transactions;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult CreateInvoice()
         {
             // Set navbar indicator location
             ViewBag.indicatorLeft = "28%";
             ViewBag.indicatorOpacity = "1";
             ViewBag.selectedPage = "2";
-
             return View();
         }
     }
